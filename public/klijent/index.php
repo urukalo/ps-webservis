0769fdd13a31f5d2bd863f0ba3d659e31d8c6cbd
Boske <Boske@Boske-PC>
2013-12-26 17:38:11 +0100

    izmene za admin deo

--- public/klijent/index.php
+++ public/klijent/index.php
@@ -14,39 +14,40 @@ $opcije_admin = array(
 try {
     $klijent = new Zend_Soap_Client(null, $opcije);
     $admin= new Zend_Soap_Client(null, $opcije_admin);
-    echo "<br/><br/> uzmi kartu ";
-    $karta = $klijent->getKarta();
-    print_r($karta);
-
-    echo "<br/><br/> ulazne stanice ";
-    var_dump($klijent->stanicaUlazna());
-
-    
-    $data = array(
-        'idKarta' => 1,
-        'idTrasa' => 0,
-        'idStanicaPolaska' => 0,
-        'idStanicaDolaska' => 0,
-        'vremePolaska' => 0,
-        'cena' => 0,
-        'aktivnost' => 0
-    );
-    $karta = (object) $data;
-    
-    echo "<br/> nova karta: ";
-    print_r($karta);
-
-    echo "<br/> sacuvaj kartu: ";
-    print_r($klijent->sacuvajKartu($karta));
-
-    echo "<br/> pronadji kartu: ";
-    print_r($klijent->pronadjiKartu(5));
-    
-    echo "<br/> daj popuste: ";
-    print_r($klijent->popusti());
+//    echo "<br/><br/> uzmi kartu ";
+//    $karta = $klijent->getKarta();
+//    print_r($karta);
+//
+//    echo "<br/><br/> ulazne stanice ";
+//    var_dump($klijent->stanicaUlazna());
+//
+//    
+//    $data = array(
+//        'idKarta' => 1,
+//        'idTrasa' => 0,
+//        'idStanicaPolaska' => 0,
+//        'idStanicaDolaska' => 0,
+//        'vremePolaska' => 0,
+//        'cena' => 0,
+//        'aktivnost' => 0
+//    );
+//    $karta = (object) $data;
+//    
+//    echo "<br/> nova karta: ";
+//    print_r($karta);
+//
+//    echo "<br/> sacuvaj kartu: ";
+//    print_r($klijent->sacuvajKartu($karta));
+//
+//    echo "<br/> pronadji kartu: ";
+//    print_r($klijent->pronadjiKartu(5));
+//    
+//    echo "<br/> daj popuste: ";
+//    print_r($klijent->popusti());
     
     echo "<br/> red voznje: ";
-    print_r($admin->getRedVoznje());
+    $r=$admin->setTrasa("Bar_Neka Stanica_300_3");
+    print_r($r);
 } catch (SoapFault $s) {
     die('Error[' . $s->faultcode . ']' . $s->faultstring);
 } catch (Exception $e) {
